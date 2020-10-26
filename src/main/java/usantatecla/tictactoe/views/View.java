ed10236b25aafedebedd921772a2a343f06b9a94
Cristofer <cristofl@ucm.es>
2020-10-26 11:15:36 +0100

    Resolved error in double dispatching

--- src/main/java/usantatecla/tictactoe/views/View.java
+++
@@ -1,9 +0,0 @@
-package usantatecla.tictactoe.views;
-
-import usantatecla.tictactoe.controllers.UseCaseController;
-
-public abstract class View {
-
-	public abstract void interact(UseCaseController controller);
-
-}
